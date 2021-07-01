0d61bb78883a18b9c4c83d384743e5ecee714946
Taha Hussain <78305343+hahatussain@users.noreply.github.com>
2021-07-01 20:45:35 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-//app.set("view engine", "ejs");
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
-server.listen(3030);
